d23551ec30d0195ca1e3dce003fa1c24ed8bde3c
Zwalker-nntc <zwalker@northernnecktech.org>
2026-08-20 17:19:26 +0000

    V2

--- src/react/MainMenu.tsx
+++ src/react/MainMenu.tsx
@@ -48,12 +48,6 @@ const MainMenuBase = ({
   optionsAction,
   githubAction,
   openFileAction,
-  versionText,
-  onVersionTextClick,
-  versionStatus,
-  versionTitle,
-  onVersionStatusClick,
-  bottomRightLinks,
   singleplayerAvailable = true,
 }: Props) => {
   const { appConfig } = useSnapshot(miscUiState)
@@ -96,28 +90,12 @@ const MainMenuBase = ({
     }
   }, [appConfig?.splashText])
 
-  if (!bottomRightLinks?.trim()) bottomRightLinks = undefined
-  // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
-  const linksParsed = bottomRightLinks?.split(/;|\n/g).map(l => {
-    const parts = l.split(':')
-    return [parts[0], parts.slice(1).join(':')]
-  }) as Array<[string, string]> | undefined
-
   const singleplayerLongPress = useLongPress(
     () => {
       window.location.href = window.location.pathname + '?sp=1'
     },
     () => singleplayerAction?.(null as any),
     { delay: 500 }
-  )
-
-  const versionLongPress = useLongPress(
-    () => {
-      const buildDate = process.env.BUILD_VERSION ? new Date(process.env.BUILD_VERSION + ':00:00.000Z') : null
-      const hoursAgo = buildDate ? Math.round((Date.now() - buildDate.getTime()) / (1000 * 60 * 60)) : null
-      alert(`BUILD DATE:\n${buildDate?.toLocaleString() || 'Development build'}${hoursAgo ? `\nBuilt ${hoursAgo} hours ago` : ''}`)
-    },
-    () => onVersionTextClick?.(),
   )
 
   const connectToServerLongPress = useLongPress(
@@ -200,57 +178,6 @@ const MainMenuBase = ({
         <CreditsBookButton />
       </div>
 
-      <div className={styles['bottom-info']}>
-        <div style={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
-          <span style={{ fontSize: 10, color: 'gray' }} {...versionLongPress}>{versionText}</span>
-          <span
-            title={`${versionTitle} (click to reload)`}
-            onClick={onVersionStatusClick}
-            className={styles['product-info']}
-          >
-            <span style={{
-              position: 'absolute',
-              left: '-9999px',
-              width: '1px',
-              height: '1px',
-              overflow: 'hidden',
-              clip: 'rect(1px, 1px, 1px, 1px)',
-              whiteSpace: 'nowrap'
-            }}>
-              Prismarine Web Client
-            </span>
-
-            V2 Presented by Sandexzx {versionStatus}
-          </span>
-        </div>
-        <span className={styles['product-description']}>
-          <div className={styles['product-link']}>
-            {linksParsed?.map(([name, link], i, arr) => {
-              if (!link.startsWith('http')) link = `https://${link}`
-              const finalLink = link
-              return <div style={{
-                color: 'lightgray',
-                fontSize: 8,
-              }}>
-                <a
-                  key={name}
-                  style={{
-                    whiteSpace: 'nowrap',
-                    cursor: 'pointer',
-                  }}
-                  onClick={(e) => {
-                    e.preventDefault()
-                    openURL(finalLink, false)
-                  }}
-                >{name}
-                </a>
-                {i < arr.length - 1 && <span style={{ marginLeft: 2 }}>·</span>}
-              </div>
-            })}
-          </div>
-          <span>{appConfig?.rightSideText}</span>
-        </span>
-      </div>
     </div>
   )
 }
